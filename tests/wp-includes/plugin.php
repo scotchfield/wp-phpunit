82ff926809c979566f39ffd26dd1e82e90640afa
scotchfield <swrittenb@gmail.com>
2015-01-22 23:04:20 -0500

    Bring over actions directory, annotate properly

--- tests/wp-includes/plugin.php
+++ tests/wp-includes/plugin.php
@@ -437,4 +437,55 @@ class Test_Plugin extends WP_UnitTestCase {
 		$this->assertFalse( doing_filter( 'something_else' ) );
 	}
 
+	/**
+	 * @covers ::has_action
+	 * @group hooks
+	 * @ticket 23265
+	 */
+	function test_callback_representations() {
+		$tag = __FUNCTION__;
+
+		$this->assertFalse( has_action( $tag ) );
+
+		add_action( $tag, array( 'Class', 'method' ) );
+
+		$this->assertEquals( 10, has_action( $tag, array( 'Class', 'method' ) ) );
+
+		$this->assertEquals( 10, has_action( $tag, 'Class::method' ) );
+	}
+
+	/**
+	 * @covers ::add_action
+	 * @covers ::do_action
+	 * @covers ::remove_action
+	 * @group hooks
+	 * @ticket 10493
+	 */
+	function test_action_closure() {
+		$tag = 'test_action_closure';
+		$closure = function($a, $b) { $GLOBALS[$a] = $b;};
+		add_action($tag, $closure, 10, 2);
+
+		$this->assertSame( 10, has_action($tag, $closure) );
+
+		$context = array( rand_str(), rand_str() );
+		do_action($tag, $context[0], $context[1]);
+
+		$this->assertSame($GLOBALS[$context[0]], $context[1]);
+
+		$tag2 = 'test_action_closure_2';
+		$closure2 = function() { $GLOBALS['closure_no_args'] = true;};
+		add_action($tag2, $closure2);
+
+		$this->assertSame( 10, has_action($tag2, $closure2) );
+
+		do_action($tag2);
+
+		$this->assertTrue($GLOBALS['closure_no_args']);
+
+		remove_action( $tag, $closure );
+		remove_action( $tag2, $closure2 );
+	}
+
+
 }
