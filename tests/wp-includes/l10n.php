83b6f43413b5507ed82b4796461b257efebaabf1
scotchfield <swrittenb@gmail.com>
2015-01-23 13:24:14 -0500

    More tests. Eventually move general/link to the right place.

---
+++ tests/wp-includes/l10n.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @group l10n
+ * @group i18n
+ */
+class Tests_L10n extends WP_UnitTestCase {
+
+	/**
+	 * @covers ::is_textdomain_loaded
+	 * @covers ::load_textdomain
+	 * @covers ::unload_textdomain
+	 */
+	function test_load_unload_textdomain() {
+		$this->assertFalse( is_textdomain_loaded( 'wp-tests-domain' ) );
+		$this->assertFalse( unload_textdomain( 'wp-tests-domain' ) );
+
+		$file = DIR_TESTDATA . '/pomo/simple.mo';
+		$this->assertTrue( load_textdomain( 'wp-tests-domain', $file ) );
+		$this->assertTrue( is_textdomain_loaded( 'wp-tests-domain' ) );
+		$this->assertTrue( unload_textdomain( 'wp-tests-domain' ) );
+		$this->assertFalse( is_textdomain_loaded( 'wp-tests-domain' ) );
+	}
+
+}
