d71a1193ad31d4a65366af4cecf75c3948291d51
scotchfield <swrittenb@gmail.com>
2015-01-23 11:20:29 -0500

    Move more tests, add coverage. Rename db to wp-db to match source.

---
+++ tests/wp-includes/comment.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * @group comment
+ */
+class Tests_Comment extends WP_UnitTestCase {
+
+	/**
+	 * @covers ::wp_update_comment
+	 */
+	function test_wp_update_comment() {
+		$post = $this->factory->post->create_and_get( array( 'post_title' => 'some-post', 'post_type' => 'post' ) );
+		$post2 = $this->factory->post->create_and_get( array( 'post_title' => 'some-post-2', 'post_type' => 'post' ) );
+		$comments = $this->factory->comment->create_post_comments( $post->ID, 5 );
+		$result = wp_update_comment( array( 'comment_ID' => $comments[0], 'comment_parent' => $comments[1] ) );
+		$this->assertEquals( 1, $result );
+		$comment = get_comment( $comments[0] );
+		$this->assertEquals( $comments[1], $comment->comment_parent );
+		$result = wp_update_comment( array( 'comment_ID' => $comments[0], 'comment_parent' => $comments[1] ) );
+		$this->assertEquals( 0, $result );
+		$result = wp_update_comment( array( 'comment_ID' => $comments[0], 'comment_post_ID' => $post2->ID ) );
+		$comment = get_comment( $comments[0] );
+		$this->assertEquals( $post2->ID, $comment->comment_post_ID );
+	}
+
+	/**
+	 * @covers ::wp_update_comment
+	 * @ticket 30627
+	 */
+	function test_wp_update_comment_updates_comment_type() {
+		$post_id = $this->factory->post->create();
+		$comment_id = $this->factory->comment->create( array( 'comment_post_ID' => $post_id ) );
+
+		wp_update_comment( array( 'comment_ID' => $comment_id, 'comment_type' => 'pingback' ) );
+
+		$comment = get_comment( $comment_id );
+		$this->assertEquals( 'pingback', $comment->comment_type );
+	}
+
+	/**
+	 * @covers ::wp_update_comment
+	 * @ticket 30307
+	 */
+	function test_wp_update_comment_updates_user_id() {
+		$post_id = $this->factory->post->create();
+		$comment_id = $this->factory->comment->create( array( 'comment_post_ID' => $post_id ) );
+
+		wp_update_comment( array( 'comment_ID' => $comment_id, 'user_id' => 1 ) );
+
+		$comment = get_comment( $comment_id );
+		$this->assertEquals( 1, $comment->user_id );
+	}
+
+	/**
+	 * @covers ::get_approved_comments
+	 */
+	public function test_get_approved_comments() {
+		$p = $this->factory->post->create();
+		$ca1 = $this->factory->comment->create( array( 'comment_post_ID' => $p, 'comment_approved' => '1' ) );
+		$ca2 = $this->factory->comment->create( array( 'comment_post_ID' => $p, 'comment_approved' => '1' ) );
+		$ca3 = $this->factory->comment->create( array( 'comment_post_ID' => $p, 'comment_approved' => '0' ) );
+		$c2 = $this->factory->comment->create( array( 'comment_post_ID' => $p, 'comment_approved' => '1', 'comment_type' => 'pingback' ) );
+		$c3 = $this->factory->comment->create( array( 'comment_post_ID' => $p, 'comment_approved' => '1', 'comment_type' => 'trackback' ) );
+		$c4 = $this->factory->comment->create( array( 'comment_post_ID' => $p, 'comment_approved' => '1', 'comment_type' => 'mario' ) );
+		$c5 = $this->factory->comment->create( array( 'comment_post_ID' => $p, 'comment_approved' => '1', 'comment_type' => 'luigi' ) );
+
+		$found = get_approved_comments( $p );
+
+		// all comments types will be returned
+		$this->assertEquals( array( $ca1, $ca2, $c2, $c3, $c4, $c5 ), wp_list_pluck( $found, 'comment_ID' ) );
+	}
+
+	/**
+	 * @covers ::wp_update_comment
+	 * @ticket 30412
+	 */
+	public function test_get_approved_comments_with_post_id_0_should_return_empty_array() {
+		$p = $this->factory->post->create();
+		$ca1 = $this->factory->comment->create( array( 'comment_post_ID' => $p, 'comment_approved' => '1' ) );
+
+		$found = get_approved_comments( 0 );
+
+		$this->assertSame( array(), $found );
+	}
+	
+}
