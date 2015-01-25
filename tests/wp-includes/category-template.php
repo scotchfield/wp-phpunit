7da3d87d685b3aeed27b6a4863839bcc5041c998
scotchfield <swrittenb@gmail.com>
2015-01-25 15:04:46 -0500

    More tests. Some tests grouped by use case, figure those out.

---
+++ tests/wp-includes/category-template.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * @group taxonomy
+ */
+class Tests_Category_Template extends WP_UnitTestCase {
+	public function test_class() {
+		$c = $this->factory->category->create();
+
+		$found = wp_list_categories( array(
+			'hide_empty' => false,
+			'echo' => false,
+		) );
+
+		$this->assertContains( 'class="cat-item cat-item-' . $c . '"', $found );
+	}
+
+	public function test_class_containing_current_cat() {
+		$c1 = $this->factory->category->create();
+		$c2 = $this->factory->category->create();
+
+		$found = wp_list_categories( array(
+			'hide_empty' => false,
+			'echo' => false,
+			'current_category' => $c2,
+		) );
+
+		$this->assertNotRegExp( '/class="[^"]*cat-item-' . $c1 . '[^"]*current-cat[^"]*"/', $found );
+		$this->assertRegExp( '/class="[^"]*cat-item-' . $c2 . '[^"]*current-cat[^"]*"/', $found );
+	}
+
+	public function test_class_containing_current_cat_parent() {
+		$c1 = $this->factory->category->create();
+		$c2 = $this->factory->category->create( array(
+			'parent' => $c1,
+		) );
+
+		$found = wp_list_categories( array(
+			'hide_empty' => false,
+			'echo' => false,
+			'current_category' => $c2,
+		) );
+
+		$this->assertRegExp( '/class="[^"]*cat-item-' . $c1 . '[^"]*current-cat-parent[^"]*"/', $found );
+		$this->assertNotRegExp( '/class="[^"]*cat-item-' . $c2 . '[^"]*current-cat-parent[^"]*"/', $found );
+	}
+
+	/**
+	 * @ticket 16792
+	 */
+	public function test_should_not_create_element_when_cat_name_is_filtered_to_empty_string() {
+		$c1 = $this->factory->category->create( array(
+			'name' => 'Test Cat 1',
+		) );
+		$c2 = $this->factory->category->create( array(
+			'name' => 'Test Cat 2',
+		) );
+
+		add_filter( 'list_cats', array( $this, 'list_cats_callback' ) );
+		$found = wp_list_categories( array(
+			'hide_empty' => false,
+			'echo' => false,
+		) );
+		remove_filter( 'list_cats', array( $this, 'list_cats_callback' ) );
+
+		$this->assertContains( "cat-item-$c2", $found );
+		$this->assertContains( 'Test Cat 2', $found );
+
+		$this->assertNotContains( "cat-item-$c1", $found );
+		$this->assertNotContains( 'Test Cat 1', $found );
+	}
+
+	public function list_cats_callback( $cat ) {
+		if ( 'Test Cat 1' === $cat ) {
+			return '';
+		}
+
+		return $cat;
+	}
+}
--- tests/wp-includes/formatting.php
+++ tests/wp-includes/formatting.php
@@ -5065,4 +5065,54 @@ class Tests_Formatting_SanitizeOrderby extends WP_UnitTestCase {
 		$this->assertEquals("5000000", zeroise(5000000, 2));
 	}
 
+	/**
+	 * Tests the model function that expects slashed data
+	 *
+	 * @group attachment
+	 * @group slashes
+	 * @ticket 21767
+	 */
+	function test_wp_insert_attachment() {
+		$this->author_id = $this->factory->user->create( array( 'role' => 'editor' ) );
+		$this->old_current_user = get_current_user_id();
+		wp_set_current_user( $this->author_id );
+
+		// it is important to test with both even and odd numbered slashes as
+		// kses does a strip-then-add slashes in some of its function calls
+		$this->slash_1 = 'String with 1 slash \\';
+		$this->slash_2 = 'String with 2 slashes \\\\';
+		$this->slash_3 = 'String with 3 slashes \\\\\\';
+		$this->slash_4 = 'String with 4 slashes \\\\\\\\';
+		$this->slash_5 = 'String with 5 slashes \\\\\\\\\\';
+		$this->slash_6 = 'String with 6 slashes \\\\\\\\\\\\';
+		$this->slash_7 = 'String with 7 slashes \\\\\\\\\\\\\\';
+
+		$id = wp_insert_attachment(array(
+			'post_status' => 'publish',
+			'post_title' => $this->slash_1,
+			'post_content_filtered' => $this->slash_3,
+			'post_excerpt' => $this->slash_5,
+			'post_type' => 'post'
+		));
+		$post = get_post( $id );
+
+		$this->assertEquals( wp_unslash( $this->slash_1 ), $post->post_title );
+		$this->assertEquals( wp_unslash( $this->slash_3 ), $post->post_content_filtered );
+		$this->assertEquals( wp_unslash( $this->slash_5 ), $post->post_excerpt );
+
+		$id = wp_insert_attachment(array(
+			'post_status' => 'publish',
+			'post_title' => $this->slash_2,
+			'post_content_filtered' => $this->slash_4,
+			'post_excerpt' => $this->slash_6,
+			'post_type' => 'post'
+		));
+		$post = get_post( $id );
+
+		$this->assertEquals( wp_unslash( $this->slash_2 ), $post->post_title );
+		$this->assertEquals( wp_unslash( $this->slash_4 ), $post->post_content_filtered );
+		$this->assertEquals( wp_unslash( $this->slash_6 ), $post->post_excerpt );
+
+		wp_set_current_user( $this->old_current_user );
+	}
 }
